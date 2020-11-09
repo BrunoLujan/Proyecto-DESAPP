0959f4aa558d2ad3ed773e159ecf98c6a49a8d45
Maria Jose <beche_1999@live.com.mx>
2020-11-09 11:55:41 -0600

    Pruebas (TestPushAndPop - testEmpty)

--- Proyecto-DESAPP/tests/Feature/ExampleTest.php
+++ Proyecto-DESAPP/tests/Feature/ExampleTest.php
@@ -18,4 +18,24 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    public function testPushAndPop()
+    {
+        $stack = [];
+        $this->assertSame(0, count($stack));
+        array_push($stack, 'foo');
+        $this->assertSame('foo', $stack[count($stack)-1]);
+        $this->assertSame(1, count($stack));
+        $this->assertSame('foo', array_pop($stack));
+        $this->assertSame(0, count($stack));
+    }
+
+    public function testEmpty()
+    {
+        $stack = [];
+        $this->assertEmpty($stack);
+        return $stack;
+    }
+
+
 }
